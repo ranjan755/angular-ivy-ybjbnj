5197842178320e1908b9c5aca1b6e963108f5ff5
ranjan755 <99010390+ranjan755@users.noreply.github.com>
2023-02-03 11:57:31 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,3 @@
-import { Component, VERSION } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  allowserver = false;
-  servercreationstatus = 'no server is created';
-  servername = 'testserver';
-  servercreated = false;
-  oncreateserver() {
-    this.servercreated = true;
-    this.servercreationstatus =
-      'server is created. name of the server is' + this.servername;
-  }
-  onupdateserver() {
-    this.servername = (<HTMLInputElement>event.target).value;
-  }
-}
+<div *ngFor="let item of users">
+    <div>{{ item.id }} - {{ item.name }}</div>
+  </div>
